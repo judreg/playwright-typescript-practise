ac3cf29c622d3aa95f75961d13af67e0df635062
Regina Judak <reginajudak@gmail.com>
2023-11-06 18:14:36 +0100

    cleanup

--- tests/guru99.spec.ts
+++ tests/guru99.spec.ts
@@ -6,21 +6,20 @@ test("Iframes and multiple tabs", async ({ page, context }) => {
 
   const pagePromise = context.waitForEvent("page");
   await page.frameLocator("#a077aa5e").locator("img").click();
+
   const newPage = await pagePromise;
   await newPage.waitForLoadState();
   expect(await newPage.title()).toEqual(
     "Selenium Live Project: FREE Real Time Project for Practice"
   );
-  console.log(await context.pages().length);
-  await context.pages()[1].close();
-  console.log(await context.pages().length);
+
+  await newPage.close();
   await page.bringToFront();
 
   await page.locator(".top-banner").getByText("Testing").click();
   const outerIframe = await page.frameLocator(
     '[id="google_ads_iframe_/24132379/INTERSTITIAL_DemoGuru99_0"]'
   );
-
   await outerIframe
     .frameLocator("#ad_iframe")
     .locator("#dismiss-button")
